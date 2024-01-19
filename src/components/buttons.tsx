aceee8e9c0220859838842b760ecfb47b16b4be0
Charles Buffington <dev.cfsb@proton.me>
2024-01-19 08:47:29 -0500

    fix layouts for blog and projects pages

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,10 +1,15 @@
-import { ArrowRight, Mail } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, Mail } from "lucide-react";
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { GitHubSVG } from "@/components/svgs";
 
-export function LinkedInButton() {
+interface LinkedInButtonProps {
+  className?: React.HTMLAttributes<ButtonProps>["className"];
+}
+
+export function LinkedInButton({ className }: LinkedInButtonProps) {
   return (
     <a href="https://www.linkedin.com/in/charles-buffington/" target="_blank">
-      <Button variant="outline" size="icon" className="p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2">
+      <Button variant="outline" size="icon" className={`${className || ""} p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2`}>
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <title>LinkedIn</title>
           <path
@@ -17,10 +22,14 @@ export function LinkedInButton() {
   )
 }
 
-export function GitHubButton() {
+interface GitHubButtonProps {
+  className?: React.HTMLAttributes<ButtonProps>["className"];
+}
+
+export function GitHubButton({ className }: GitHubButtonProps) {
   return (
     <a href="https://github.com/C41M50N" target="_blank">
-      <Button variant="outline" size="icon" className="p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2">
+      <Button variant="outline" size="icon" className={`${className || ""} p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2`}>
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <title>GitHub</title>
           <path
@@ -33,10 +42,14 @@ export function GitHubButton() {
   )
 }
 
-export function MailButton() {
+interface MailButtonProps {
+  className?: React.HTMLAttributes<ButtonProps>["className"];
+}
+
+export function MailButton({ className }: MailButtonProps) {
   return (
     <a href="mailto:hello.cfsb@proton.me">
-      <Button variant="outline" size="icon" className="p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2">
+      <Button variant="outline" size="icon" className={`${className || ""} p-1.5 opacity-70 hover:opacity-90 transition-opacity border-2`}>
         <Mail strokeWidth={1.75} />
       </Button>
     </a>
@@ -46,16 +59,64 @@ export function MailButton() {
 interface AnimatedLinkButtonProps {
   label: string;
   href: string;
+  direction?: "fwd" | "bkwd";
 }
 
 export function AnimatedLinkButton(props: AnimatedLinkButtonProps) {
+  if (props.direction === undefined || props.direction === "fwd") {
+    return (
+      <a href={props.href} className="group/test">
+        <Button variant="outline" className="text-white/70 group-hover/test:text-white/95">
+          <span className="group-hover/test:underline group-hover/test:underline-offset-1">{props.label}</span>
+          <div className="mx-0.5" />
+          <div className="group-hover/test:translate-x-1 duration-200 ease-in">
+            <ArrowRight strokeWidth={1.0} size={24} />
+          </div>
+        </Button>
+      </a>
+    )
+  } else {
+    return (
+      <a href={props.href} className="group/test">
+        <Button variant="outline" className="text-white/70 group-hover/test:text-white/95">
+          <div className="group-hover/test:-translate-x-1 duration-200 ease-in">
+            <ArrowLeft strokeWidth={1.0} size={24} />
+          </div>
+          <div className="mx-0.5" />
+          <span className="group-hover/test:underline group-hover/test:underline-offset-1">{props.label}</span>
+        </Button>
+      </a>
+    )
+  }
+}
+
+interface LiveProjectButtonProps {
+  href: string;
+  size?: ButtonProps["size"];
+}
+
+export function LiveProjectButton({ href, size }: LiveProjectButtonProps) {
   return (
-    <a href={props.href} className="group/test">
-      <Button variant="outline" className="text-white/70 group-hover/test:text-white/95">
-        <span className="mr-1 group-hover/test:underline group-hover/test:underline-offset-1">{props.label}</span>
-        <div className="group-hover/test:translate-x-1 duration-200 ease-in">
-          <ArrowRight strokeWidth={1.0} size={24} />
-        </div>
+    <a href={href}>
+      <Button variant="outline" size={size || "sm"}>
+        <div className="w-3 h-3 bg-[#63b681] animate-pulse border-2 border-black rounded-full" />
+        <span className="pl-2 tracking-wide">Live</span>
+      </Button>
+    </a>
+  )
+}
+
+interface GitHubProjectButtonProps {
+  href: string;
+  size?: ButtonProps["size"]
+}
+
+export function GitHubProjectButton({ href, size }: GitHubProjectButtonProps) {
+  return (
+    <a href={href}>
+      <Button variant="outline" size={size || "sm"}>
+        <GitHubSVG className='w-5' />
+        <span className="pl-2 tracking-wide">GitHub</span>
       </Button>
     </a>
   )
