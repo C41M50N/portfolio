0eff5150d99cf9158ffc360a7e2d80d34ffd416e
Charles Buffington <dev.cfsb@proton.me>
2024-11-13 00:13:03 -0500

    visual improvements

--- src/components/projects/tech-badge.tsx
+++ src/components/projects/tech-badge.tsx
@@ -1,15 +1,20 @@
 import type { TechData } from "@/lib/data"
+import { cn } from "@/lib/utils";
 
 type Props = {
   data: TechData;
+  active?: boolean;
 }
 
-export function TechBadge({ data }: Props) {
+export function TechBadge({ data, active = false }: Props) {
   return (
     <div className="group bg-[#161616] py-1 px-1.5 flex flex-row gap-2 rounded-md border border-zinc-800">
       <img
         src={`/tech-logos/${data.img}`}
-        className="size-4 transition-all duration-500 filter grayscale group-hover:filter-none"
+        className={cn(
+          "size-4 transition-all duration-500",
+          !active && "filter grayscale group-hover:filter-none"
+        )}
       />
       <span className="font-medium text-xs text-zinc-100 cursor-default">
         {data.label}
